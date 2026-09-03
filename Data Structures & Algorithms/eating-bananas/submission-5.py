0d516c7aaba22794f96f587234845b63e297756f
yp <138333687+yaseenp24@users.noreply.github.com>
2026-09-02 23:31:14 -0700

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.py
@@ -0,0 +1,35 @@
+# class Solution:
+#     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+#         left, right = 1, max(piles)
+#         res = right
+#         while left < right:
+#             mid = (left + right) // 2
+#             total = 0
+#             for p in piles:
+#                 total += math.ceil(p / mid)
+#             if total <= h:
+#                 res = min(res, mid)
+#                 right = mid - 1
+#             else:
+#                 left = mid + 1
+#         return res
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left, right = 1, max(piles)
+        res = right
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            total = 0
+
+            for p in piles:
+                total += math.ceil(p / mid)
+
+            if total <= h:
+                res = min(res, mid)
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return res
